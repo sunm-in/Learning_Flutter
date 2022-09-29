c8c211db57f273c5a58259de5bb365763d8e6d3d
sunmin <sunm309@naver.com>
2022-09-29 22:13:05 +0900

    custom widget

--- contact/lib/main.dart
+++ contact/lib/main.dart
@@ -10,75 +10,22 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(
-        leading: Icon(Icons.search),
-        title: Text(
-          '구로구',
-        ),
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.notifications_none),
-          ),
-          IconButton(onPressed: () {}, icon: Icon(Icons.menu)),
-        ],
+      home: Scaffold(
+        appBar: AppBar(),
+        body: Item(),
       ),
-      body: Container(
-        height: 150,
-        padding: EdgeInsets.all(10),
-        child: Row(
-          children: [
-            ClipRRect(
-              borderRadius: BorderRadius.circular(20),
-              child: Image.asset(
-                'assets/img_5835.jpg',
-                fit: BoxFit.fill,
-                width: 150,
-              ),
-            ),
-            Expanded(
-              child: Container(
-                padding: EdgeInsets.all(10),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      '토이푸들 게시물 제목 토이푸들 게시물 제목 토이푸들 게시물 제목 토이푸들 게시물',
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.w700,
-                      ),
-                    ),
-                    Text(
-                      '구로구 개봉동 10분 전',
-                      style: TextStyle(
-                          fontSize: 12,
-                          fontWeight: FontWeight.w500,
-                          color: Colors.grey),
-                    ),
-                    Text(
-                      '이름이름',
-                      style:
-                          TextStyle(fontSize: 14, fontWeight: FontWeight.w600),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        Icon(
-                          Icons.favorite_border,
-                          color: Colors.grey,
-                        ),
-                        Text('4')
-                      ],
-                    )
-                  ],
-                ),
-              ),
-            )
-          ],
-        ),
-      ),
-    ));
+    );
   }
 }
+
+class Item extends StatelessWidget {
+  const Item({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      child: Text('안녕하세요'),
+    );
+  }
+}
+
