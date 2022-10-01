ca4ee4576ade938dc0328f667569c6c89113ab1b
sunmin <sunm309@naver.com>
2022-10-01 18:49:28 +0900

    리스트 반복, customBottomAppBar

--- contact/lib/main.dart
+++ contact/lib/main.dart
@@ -1,28 +1,56 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  MyApp({Key? key}) : super(key: key);
+
+  var a = 1;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(),
-        body: ListView(
-          children: [
-            Text('안녕하세요'),
-            Text('안녕하세요'),
-            Text('안녕하세요'),
-            Text('안녕하세요'),
-            Text('안녕하세요'),
-          ],
+        floatingActionButton: FloatingActionButton(
+          child: Text('+'),
+          onPressed: (){},
         ),
+        appBar: AppBar(),
+        bottomNavigationBar: CustomBottomAppBar(),
+        body: ListView.builder(
+            itemCount: 5,
+            itemBuilder: (context, i) {
+              print(i); // 출력문
+              return ListTile(
+                leading: ClipRRect(
+                    borderRadius: BorderRadius.circular(30),
+                    child: Image.asset('assets/img_5835.jpg')),
+                title: Text('푸들이'),
+              );
+            }),
       ),
     );
   }
 }
 
+class CustomBottomAppBar extends StatelessWidget {
+  const CustomBottomAppBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 60,
+      color: Color.fromRGBO(200, 200, 200, 0.2),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Icon(Icons.phone),
+          Icon(Icons.message),
+          Icon(Icons.contact_page),
+        ],
+      ),
+    );
+  }
+}
