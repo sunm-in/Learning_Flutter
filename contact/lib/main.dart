6e7273a7c64f286a525168060961cc7f07b4e261
sunmin <sunm309@naver.com>
2022-10-05 22:43:29 +0900

    state 전달, list 자료형 다루기

--- contact/lib/main.dart
+++ contact/lib/main.dart
@@ -13,7 +13,15 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   var name = ['푸들이0', '푸들이1', '푸들이2'];
-  var likeTotal = [0, 0, 0, 0, 0];
+  var likeTotal = [0, 0, 0];
+  int total = 3;
+
+  void incrementTotal() {
+    setState(() {
+      total++;
+      Navigator.pop(context);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,11 +31,13 @@ class _MyAppState extends State<MyApp> {
           showDialog(
               context: context,
               builder: (context) {
-                return DialogWidget();
+                return DialogWidget(state: total, addOne: incrementTotal);
               });
         },
       ),
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text(total.toString()),
+      ),
       bottomNavigationBar: CustomBottomAppBar(),
       body: ListView.builder(
           itemCount: 3,
@@ -42,20 +52,47 @@ class _MyAppState extends State<MyApp> {
 }
 
 class DialogWidget extends StatelessWidget {
-  const DialogWidget({Key? key}) : super(key: key);
+  DialogWidget({Key? key, this.state, required this.addOne}) : super(key: key);
+  final state;
+  final Function() addOne;
+
+  var inputData = TextEditingController();
+
+  Map mapData = {
+    'name1': '푸들',
+    'name2': '푸들푸들',
+    'name3': '푸들푸들푸들',
+  };
+  Map inputData2 = {};
+
+  // Map inputData2 = new Map();
 
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
       title: Text('Contact'),
-      content: TextField(),
+      content: TextField(
+        // controller: inputData,
+        onChanged: (text) {
+          inputData2['name4'] = text;
+          mapData.addAll(inputData2);
+          print(inputData2);
+          print(mapData);
+        },
+      ),
       actions: [
+        Text(state.toString()),
         TextButton(
             onPressed: () {
-              Navigator.pop(context);
+              // Navigator.pop(context);
+              print(inputData.text);
             },
             child: Text('Cancel')),
-        TextButton(onPressed: () {}, child: Text('OK'))
+        TextButton(
+            onPressed: () {
+              addOne();
+            },
+            child: Text('OK'))
       ],
     );
   }
