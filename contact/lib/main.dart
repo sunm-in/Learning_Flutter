bd38736842fc4024ae947f06fdf8a710d3555430
sunmin <sunm309@naver.com>
2022-10-09 22:10:04 +0900

    getPermission()

--- contact/lib/main.dart
+++ contact/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 void main() {
   runApp(MaterialApp(home: MyApp()));
@@ -12,6 +13,28 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  // Dart 특징에서 오래걸리는 줄은 넘어가고 다음 줄 실행하려고 한다.
+  // await 붙으면 다음 줄 실행안하고 기다려준다.
+  getPermission() async {
+    var status = await Permission.contacts.status; // 연락처 권한 줬는지 여부
+    if (status.isGranted) {
+      // 허락했을 때 실행할 코드
+      print('허락됨');
+    } else if (status.isDenied) {
+      // 거절했을 때 실행할 코드
+      print('거절됨');
+      Permission.contacts.request(); // 요청 팝업 띄우는 코드
+      // openAppSettings(); // 앱설정화면을 켜준다. 거절당하면 유저가 앱설정에서 직접 권한 켜야함.
+    }
+  }
+
+  // initState 안에 적은 코드는 위젯 로드될 때 한 번 실행된다.
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   getPermission();
+  // }
+
   var name = ['푸들이0', '푸들이1', '푸들이2'];
   var likeTotal = [0, 0, 0];
   int total = 3;
@@ -37,7 +60,8 @@ class _MyAppState extends State<MyApp> {
           showDialog(
               context: context,
               builder: (context) {
-                return DialogWidget(state: total,
+                return DialogWidget(
+                    state: total,
                     nameList: name,
                     addOne: addOne,
                     appendPoodle: appendPoodle);
@@ -46,13 +70,20 @@ class _MyAppState extends State<MyApp> {
       ),
       appBar: AppBar(
         title: Text(total.toString()),
+        actions: [
+          IconButton(
+              onPressed: () {
+                getPermission();
+              },
+              icon: Icon(Icons.contacts))
+        ],
       ),
       bottomNavigationBar: CustomBottomAppBar(),
       body: ListView.builder(
           itemCount: name.length,
           itemBuilder: (context, i) {
             return ListTile(
-              leading: Image.asset('assets/img_5835.jpg'),
+              leading: Image.asset('assets/IMG_5835.JPG'),
               title: Text(name[i]),
             );
           }),
@@ -62,7 +93,11 @@ class _MyAppState extends State<MyApp> {
 
 class DialogWidget extends StatelessWidget {
   DialogWidget(
-      {Key? key, this.state, this.nameList, required this.addOne, this.appendPoodle})
+      {Key? key,
+      this.state,
+      this.nameList,
+      required this.addOne,
+      this.appendPoodle})
       : super(key: key);
   final state;
   var nameList;
